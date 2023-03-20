627d607ede8514b15e7be327f973f297be17c196
Roark McKibben <roarkmckibben@gmail.com>
2023-03-20 17:16:16 -0500

    Documented Authenticator

--- src/main/java/io/github/djtpj/authenticator/Authenticator.java
+++ src/main/java/io/github/djtpj/authenticator/Authenticator.java
@@ -4,13 +4,25 @@ import io.github.djtpj.trait.Ability;
 import lombok.Getter;
 import org.bukkit.event.Event;
 
+/** An Authenticator is a reusable way to check if an Ability should run on an event
+ * @param <T> the Event to Authenticate
+ * @see Event
+ * @see Ability
+ */
 public abstract class Authenticator<T extends Event> {
     @Getter
     private final Ability associatedAbility;
 
+    /**
+     * @param associatedAbility the ability to associate with the Authenticator
+     */
     protected Authenticator(Ability associatedAbility) {
         this.associatedAbility = associatedAbility;
     }
 
+    /** Verifies weather a trait should run on an event
+     * @param event The event to authenticate
+     * @return whether a trait should run on this event
+     */
     public abstract boolean authenticate(T event);
 }
