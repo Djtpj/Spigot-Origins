4f5901086fafc2abb40b8fdc524295a4c5c4d70f
Roark McKibben <roarkmckibben@gmail.com>
2023-03-20 16:56:56 -0500

    Tweaked and optimized RunnableAbility

--- src/main/java/io/github/djtpj/trait/traits/RunnableAbility.java
+++ src/main/java/io/github/djtpj/trait/traits/RunnableAbility.java
@@ -10,36 +10,42 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Map;
 
 import static io.github.djtpj.origin.Main.plugin;
 
+/** A RunnableAbility is an ability that runs over and over again every ten ticks */
 public abstract class RunnableAbility <T extends Event> extends Ability<T> {
     public static final String ID = "runnable-ability";
 
-    private final static HashSet<RunnableAbility> abilities = new HashSet<>();
-
-    static {
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, RunnableAbility::tick, 0, 10);
+    protected RunnableAbility(String name, String description, ChatColor color, Material material, Type type, Authenticator<T> authenticator) {
+        this(name, description, color, material, type, authenticator, 0, 10);
     }
 
-    protected RunnableAbility(String name, String description, ChatColor color, Material material, Type type, Authenticator<T> authenticator) {
+    protected RunnableAbility(String name, String description, ChatColor color, Material material, Type type, Authenticator<T> authenticator,  int loop) {
+        this(name, description, color, material, type, authenticator, 0, loop);
+    }
+
+    protected RunnableAbility(String name, String description, ChatColor color, Material material, Type type, Authenticator<T> authenticator, int delay, int loop) {
         super(name, description, color, material, type);
 
-        abilities.add(this);
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, this::tick, delay, loop);
     }
 
-    private static void tick() {
-        for (RunnableAbility ability : abilities) {
-            for (Player player : ability.getApplicablePlayers()) {
-                ability.onTick(player);
-            }
+    private void tick() {
+        for (Player player : getApplicablePlayers()) {
+            onTick(player);
         }
     }
 
+    /** The method to run on each player every ten ticks
+     * @param player The player to run the method on
+     */
     protected abstract void onTick(Player player);
 
+    /** Returns the players with an origin that posses this ability
+     * @return the players that have this ability
+     */
     protected Player[] getApplicablePlayers() {
         // Dear god java 8 is weird
         return PlayerManager.getInstance().getPlayerOriginMap().entrySet().stream()
