35f4a48bde20bcc2f1306473b289425ba962663f
Roark McKibben <roarkmckibben@gmail.com>
2023-03-20 17:44:24 -0500

    Documented IllDefinedTraitException.java

--- src/main/java/io/github/djtpj/trait/IllDefinedTraitException.java
+++ src/main/java/io/github/djtpj/trait/IllDefinedTraitException.java
@@ -1,5 +1,8 @@
 package io.github.djtpj.trait;
 
+/**
+ * Thrown when a {@link Trait} is not fully defined
+ */
 public class IllDefinedTraitException extends Exception {
     public IllDefinedTraitException(String message) {
         super(message);
