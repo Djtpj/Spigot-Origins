b1fa5edf1b80b45e793137259f86bb4d15f9d0cc
Roark McKibben <roarkmckibben@gmail.com>
2023-03-28 21:02:29 -0500

    Created Photosynthesis Ability

---
+++ src/main/java/io/github/djtpj/trait/traits/Photosynthesis.java
@@ -0,0 +1,57 @@
+package io.github.djtpj.trait.traits;
+
+import io.github.djtpj.authenticator.authenticators.PlayerAuthenticator;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+
+import java.util.Objects;
+
+public class Photosynthesis extends RunnableAbility {
+    public static final String ID = "photosynthesis";
+
+    public Photosynthesis() {
+        super("Photosynthesis", "You are only able to sustain yourself off of the sunlight alone by raising your head towards the sky.", ChatColor.YELLOW, Material.COOKED_BEEF, Type.POSITIVE);
+    }
+
+    @Override
+    protected void onTick(Player player) {
+        float pitch = player.getLocation().getPitch();
+
+        final float REQUIRED_ANGLE = -45;
+        if (pitch < REQUIRED_ANGLE && isSunny(player) && DimensionSpawn.Dimension.OVERWORLD.playerIn(player)) {
+            int blockY = Objects.requireNonNull(player.getLocation().getWorld()).getHighestBlockYAt(player.getLocation());
+
+            if (blockY <= player.getLocation().getY()) {
+                // Do not let the food level exceed the ceiling
+                final int FOOD_LEVEL_CEILING = 20, AMOUNT_TO_ADD = 2;
+                int newLevel = Math.min(FOOD_LEVEL_CEILING, player.getFoodLevel() + AMOUNT_TO_ADD);
+
+                player.setFoodLevel(newLevel);
+            }
+        }
+    }
+
+    @EventHandler
+    public void preventEat(PlayerItemConsumeEvent event) {
+        if (!new PlayerAuthenticator(this).authenticate(event)) return;
+
+        if (event.getItem().getType().isEdible()) {
+            event.setCancelled(true);
+        }
+    }
+
+    private static boolean isSunny(Player player) {
+        final long DAY_TIME = 12300, NIGHT_TIME = 23850;
+        long time = player.getWorld().getTime();
+
+        boolean isDay = time < DAY_TIME || time > NIGHT_TIME;
+        if (isDay) {
+            return !player.getWorld().hasStorm() && !player.getWorld().isThundering();
+        }
+
+        return false;
+    }
+}
